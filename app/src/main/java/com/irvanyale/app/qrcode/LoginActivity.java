f3889c7f6f23a1e81021bdebd95dd0c6140f2727
irvanyale <irvanyale14@gmail.com>
2017-04-01 18:30:38 +0700

    init project

---
+++ app/src/main/java/com/irvanyale/app/qrcode/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.irvanyale.app.qrcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
